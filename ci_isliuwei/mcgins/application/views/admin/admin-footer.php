4117d1e25734625ddfc1ddb72a9312b21841fce0
SY199505 <SY199505@live.com>
2016-08-29 21:02:10 +0800

    并不知道为什么更新
    之前那个就不好使，不开XAMPP都能进入首页，但不能进入其他页面

--- ci_isliuwei/mcgins/application/views/admin/admin-footer.php
+++
@@ -1,4 +0,0 @@
-<footer class="admin-content-footer">
-      <hr>
-      <p class="am-padding-left">© 2016 McGins English Education, Inc. Background Management System. Power by Weichuang.</p>
-</footer>
